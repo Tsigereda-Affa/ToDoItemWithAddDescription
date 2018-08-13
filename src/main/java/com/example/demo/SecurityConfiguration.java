7930a52a541bc22330053145f99e7591cd426c15
tsigereda-affa <affa16.a@gmail.com>
2018-08-13 16:13:02 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/SecurityConfiguration.java
@@ -0,0 +1,86 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.util.AntPathMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Bean
+   public static PasswordEncoder encoder(){
+      return new BCryptPasswordEncoder();
+        }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //Restricts access to routes
+        http.authorizeRequests()
+                .antMatchers("/")
+                .access("hasAnyAuthority('USER', 'ADMIN')")
+                .antMatchers("/admin").access("hasAuthority('ADMIN')")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/login").permitAll()
+                .and()
+                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/login").permitAll()
+                .and().httpBasic();
+    }
+        @Override
+        protected void configure(AuthenticationManagerBuilder auth)throws Exception{
+        auth.inMemoryAuthentication().
+                withUser("dave").password(encoder().encode("password")).authorities("ADMIN").and().
+                withUser("user").password(encoder().encode("password")).authorities("USER").and().passwordEncoder(encoder());
+
+        }
+
+//        @Autowired
+//    private SSUserDetailsService userDetailsService;
+//        @Autowired
+//    private UserRepository userRepository;
+//
+//        @Override
+//    public UserDetailsService userDetailsServiceBean() throws Exception {
+//            return new SSUserDetailsService(userRepository);
+//        }
+//        @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//            http
+//                    .authorizeRequests()
+//                    .antMatchers("/", "/h2-console/**").permitAll()
+//                    .anyRequest().authenticated()
+//                    .and()
+//                    .formLogin().loginPage("/login").permitAll()
+//                    .and()
+//                    .logout()
+//                    .logoutRequestMatcher(
+//                            new AntPathRequestMatcher("/logout"))
+//                    .logoutSuccessUrl("/login").permitAll().permitAll()
+//                    .and()
+//                    .httpBasic();
+//            http
+//                    .csrf().disable();
+//            http
+//                    .headers().frameOptions().disable();
+//
+//        }
+//        @Override
+//    protected void configure(AuthenticationManagerBuilder auth)
+//                throws Exception {
+//            auth
+//                    .userDetailsService(userDetailsServiceBean());
+        }
+
+
